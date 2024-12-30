4f7fadc4a828fc84ce842fb75bf77cd5b8a42d19
RadiTEFA <radingoding@gmail.com>
2024-12-30 17:38:06 +0700

    Add files via upload

---
+++ pages/Ekstrakulikuler/index.vue
@@ -0,0 +1,281 @@
+<template>
+    <div class="container-fluid pt-3 pb-3 bg-secondary">
+        <div class="row py-3 font">
+            <div class="col">
+                <h4 class="text-white text-center">Organisasi</h4>
+            </div>
+        </div>
+        <div class="row Organisasi row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/MPK.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>MAJLIS PERMUSYAWARATAN KELAS</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/Osis.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>OSIS</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoSRL.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>SAFETY RIDING</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+        <div class="row py-3 font">
+            <div class="col">
+                <h4 class="text-white text-center">Ekstrakulikuler</h4>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
+            <div class="col">
+                <div class="card h-100 col-items">
+                    <img src="/gambar/PASKIBRA.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>PASKIBRA</p>
+                    </div>
+                    <a href="https://smkn4tasikmalaya.vercel.app/Ekstrakulikuler/Paskibra" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoEnglishClub.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>ENGLISH CLUB</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoPerisaiDiri.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>PERISAI DIRI</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100 bg-white">
+                    <img src="/gambar/LogoPATROLIKEAMANANSEKOLAH.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>PATROLI KEAMANAN SEKOLAH</p>
+                    </div>
+                    <a href="https://smkn4tasikmalaya.vercel.app/Ekstrakulikuler/PKS" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoPramuka.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>PRAMUKA</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoPMR.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>PALANG MERAH REMAJA</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoBuluTangkis.png" class="card-img-top img-organisasi-eskul1" alt="...">
+                    <div class="card-body">
+                        <p>BULU TANGKIS</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoFUTSAL.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>FUTSAL</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoITCLUB.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>IT CLUB</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoPLH.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>PECINTA LINGKUNGAN HIDUP</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoTari.png" class="card-img-top img-organisasi-eskul1" alt="...">
+                    <div class="card-body">
+                        <p>SENI</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoVOLI.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>VOLI</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoLASKARPUSTAKA.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>LASKAR PUSTAKA</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoBasket.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>BASKET</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoCinema.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>CINEMATOGRAPHY</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoIrma.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>IKATAN REMAJA MASJID</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="/gambar/LogoSEPAKBOLA.png" class="card-img-top img-organisasi-eskul" alt="...">
+                    <div class="card-body">
+                        <p>SEPAK BOLA</p>
+                    </div>
+                    <a href="#" class="btn btn-primary">Lihat Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style setup>
+@import url('https://fonts.googleapis.com/css2?family=Tinos&display=swap');
+.img-organisasi-eskul {
+    filter: brightness(90%) contrast(100%); /* Sesuaikan nilai */
+}
+@media (max-width: 575.98px) { 
+    .img-organisasi-eskul {
+        max-width:600px;
+        height: auto;
+    }
+    .img-organisasi-eskul1 {
+        max-width:600px;
+        height: auto;
+    }
+}
+@media (max-width: 767.98px) { 
+    .img-organisasi-eskul {
+        max-width:600px;
+        height: auto;
+    }
+    .img-organisasi-eskul1 {
+        max-width:600px;
+        height: auto;
+    }
+}
+@media (max-width: 991.98px) { 
+    .img-organisasi-eskul {
+        max-width:600px;
+        height: auto;
+    }
+    .img-organisasi-eskul1 {
+        max-width:600px;
+        height: auto;
+    }
+}
+@media (max-width: 1199.98px) { 
+    .img-organisasi-eskul {
+        max-width:600px;
+        height: auto;
+    }
+    .img-organisasi-eskul1 {
+        max-width:600px;
+        height: auto;
+    }
+}
+@media (max-width: 1399.98px) { 
+    .img-organisasi-eskul {
+        max-width:600px;
+        height: auto;
+    }
+    .img-organisasi-eskul1 {
+        max-width:600px;
+        height: auto;
+    }
+}
+.font h4 {
+    font-size: 75px;
+    margin-bottom: 10px;
+    color: #007bff;
+    font-family: "Beau Rivage", serif;
+}
+.card a {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    padding: 10px;
+    border-radius: 5px;
+    font-size: 14px;
+    cursor: pointer;
+    margin: 10px 15px;
+    align-self: center;
+}
+
+/* Efek hover pada tombol */
+.card button:hover {
+    background-color: #0056b3;
+}
+.card p {
+    font-weight: bold;
+    text-align: center;
+}
+</style>
