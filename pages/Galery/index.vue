ea61d54d0faaabd99821097baaaa5f99e94a311e
RadiTEFA <radingoding@gmail.com>
2024-12-30 19:09:05 +0700

    Update index.vue

--- pages/Galery/index.vue
+++ pages/Galery/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <p>Images</p>
+  <div class="row py-3 font">
+        <div class="col">
+          <h4 class="text-white text-center">Paskibra</h4>
+        </div>
+      </div>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 g-3">
       <div class="col">
         <img src="/gambar/EC1.jpg" class="img-thumbnail" alt="...">
@@ -48,3 +52,14 @@
       </div>
     </div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Beau+Rivage&display=swap');
+
+.font h4 {
+  font-size: 75px;
+  margin-bottom: 10px;
+  color: #007bff;
+  font-family: "Beau Rivage", serif;
+}
+</style>
