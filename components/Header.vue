7ff2167dcc8b8ed4eef36154cca074d00413c607
RadiTEFA <radingoding@gmail.com>
2025-01-02 13:32:42 +0700

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -22,9 +22,6 @@
             <li class="nav-item">
               <a class="nav-link" href="https://smkn4tasikmalaya.vercel.app/Galery">Galery</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
           </ul>
         </div>
       </div>
