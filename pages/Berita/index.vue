b02bad97519b87fd1b738a9d63c1c89fcbb9df34
RadiTEFA <radingoding@gmail.com>
2025-01-02 13:25:49 +0700

    Add files via upload

---
+++ pages/Berita/index.vue
@@ -0,0 +1,160 @@
+<template>
+    <img src="/gambar/GerbangSekolah.png" class="img-fluid" alt="..." style="width: 100vw;">
+    <div class="container-fluid bg">
+        <div class="font text-center">
+            <p>Berita</p>
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action rounded-0 bgIndexBerita">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/gambar/LogoLulus.jpg" alt="" style="height: 6vh;" class="mx-auto d-block">
+                            </div>
+                            <div class="col-6">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Pengumuman Kelulusan Siswa Tahun Pelajaran 2023/2024</h5>
+                                </div>
+                                <div>
+                                    <small class="text-muted">SMK Negeri 4 Tasikmlaya</small>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <small class="text-muted float-end">06-05-2024</small>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action rounded-0 bgIndexBerita">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/gambar/PPDBJabar.jpg" alt="" style="height: 5vh;" class="mx-auto d-block">
+                            </div>
+                            <div class="col-6">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Hasil Seleksi PPDB Tahap 2 Tahun 2020</h5>
+                                </div>
+                                <div>
+                                    <small class="text-muted">Taufik Hidayat</small>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <small class="text-muted float-end">08-07-2020</small>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action rounded-0 bgIndexBerita">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/gambar/PPDBJabar.jpg" alt="" style="height: 5vh;" class="mx-auto d-block">
+                            </div>
+                            <div class="col-6">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h5 class="mb-1">Daftar Ulang PPDB SMK Negeri 4 Tasikmalaya Tahap 1 Tahun 2020</h5>
+                                </div>
+                                <div>
+                                    <small class="text-muted">Muhammad Hilmi Ahadiat</small>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <small class="text-muted float-end">23-06-2024</small>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action rounded-0 bgBeritaPilihan">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/gambar/LogoLulus.jpg" alt="" style="height: 5vh;" class="mx-auto d-block">
+                            </div>
+                            <div class="col-6">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h6 class="mb-1">Pengumuman Kelulusan Siswa Tahun Pelajaran 2023/2024</h6>
+                                </div>
+                                <div>
+                                    <small class="text-muted">SMK Negeri 4 Tasikmlaya</small>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <small class="text-muted float-end">06-05-2024</small>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action rounded-0 bgBeritaPilihan">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/gambar/PPDBJabar.jpg" alt="" style="height: 4vh;" class="mx-auto d-block">
+                            </div>
+                            <div class="col-6">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h6 class="mb-1">Hasil Seleksi PPDB Tahap 2 Tahun 2020</h6>
+                                </div>
+                                <div>
+                                    <small class="text-muted">Taufik Hidayat</small>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <small class="text-muted float-end">08-07-2020</small>
+                            </div>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action rounded-0 bgBeritaPilihan">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/gambar/PPDBJabar.jpg" alt="" style="height: 4vh;" class="mx-auto d-block">
+                            </div>
+                            <div class="col-6">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <h6 class="mb-1">Daftar Ulang PPDB SMK Negeri 4 Tasikmalaya Tahap 1 Tahun 2020</h6>
+                                </div>
+                                <div>
+                                    <small class="text-muted">Muhammad Hilmi Ahadiat</small>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <small class="text-muted float-end">23-06-2024</small>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Beau+Rivage&display=swap');
+
+.bg {
+  background-color: #EAE7DD;
+}
+
+.bg .font p {
+  font-size: 4rem;
+  padding: 40px 0px 10px 0px;
+  font-weight: bold;
+  font-family: "Beau Rivage", serif;
+}
+.bgIndexBerita  {
+    background-color: #CDCDC0;
+}
+.bgIndexBerita h5  {
+    color: #00008B;
+}
+.bgIndexBerita h5:hover  {
+    color: #FF0000;
+}
+.bgBeritaPilihan  {
+    background-color: #CDCDC0;
+}
+.bgBeritaPilihan h6  {
+    color: #00008B;
+}
+.bgBeritaPilihan h6:hover  {
+    color: #FF0000;
+}
+
+</style>
