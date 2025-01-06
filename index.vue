5a0f1b9e4c625dbf6106e171c177aa16b1577486
RadiTEFA <radingoding@gmail.com>
2025-01-06 14:46:51 +0700

    Add files via upload

---
+++ index.vue
@@ -0,0 +1,587 @@
+<template>
+  <img src="/gambar/FotoBersamaGuruGuru.jpg" class="img-fluid" />
+  <div class="container-fluid bgSambutan">
+    <div class="font text-center">
+      <p>Sambutan Kepala Sekolah</p>
+    </div>
+    <div class="row">
+      <div class="col-xxl-4">
+        <img src="/gambar/Kurniawan.png" class="img-fluid mx-auto d-block" />
+      </div>
+      <div class="col-xxl-8 text-sambutan">
+        <p>Assallamuallaikum Wr.Wb</p>
+        <p>
+          Segala puji dan syukur kita panjatkan kehadirat Allah SWT, semoga kita
+          semua ada dalam lindungan-nya. dan atas perkenan-Nya kami pula dapat
+          menghadirkan Website SMK Negeri 4 Tasikmalaya ini.
+        </p>
+        <p>
+          Kami berharap dengan adanya Website di SMK Negeri 4 Tasikmalaya ini
+          para pengunjung dapat mengenal lebih jauh tentang sekolah kami
+          sehingga dapat mempererat tali silaturahmi antar sekolah dengan
+          masyarakat demi kemajuan kira bersama. Tiada gading yang tak retak,
+          website kami ini masih dalam proses pengembangan, masih banyak
+          kekurangan yang harus kami perbaiki. Kritik dan sarannya yang
+          membangun sangat kami harapkan untuk pengembangan ke depan.
+        </p>
+        <p>Wassallamuallaikum Wr.wb</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bgInformasi justify-content-center">
+    <div class="judul-informasi text-center justify-content-center">
+      <p>Informasi</p>
+    </div>
+    <div class="row Organisasi row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-4 g-3">
+      <div class="col">
+        <div class="info-box text-center">
+          <h2>Siswa</h2>
+          <p>1200</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="info-box text-center">
+          <h2>Guru</h2>
+          <p>60</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="info-box text-center">
+          <h2>Staff TU</h2>
+          <p>10</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="info-box text-center">
+          <h2>Ruangan</h2>
+          <p>34</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bgGalery">
+    <div class="judul-Galery text-center justify-content-center">
+      <p>Galery</p>
+    </div>
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-4 row-cols-xl-4 row-cols-xxl-4">
+      <div class="col">
+        <img src="/gambar/EC1.jpg" class="img-thumbnail" alt="..." />
+      </div>
+      <div class="col">
+        <img src="/gambar/EC1.jpg" class="img-thumbnail" alt="..." />
+      </div>
+      <div class="col">
+        <img src="/gambar/EC1.jpg" class="img-thumbnail" alt="..." />
+      </div>
+      <div class="col">
+        <img src="/gambar/EC1.jpg" class="img-thumbnail" alt="..." />
+      </div>
+    </div>
+  </div>
+  <div class="bgButton pt-3 pb-3">
+    <a href="https://smkn4tasikmalaya.vercel.app/Galery/" class="view-more btn">Lihat Galery Lainnya</a>
+  </div>
+  <div class="container-fluid bgBerita">
+    <div class="judul-Berita text-center justify-content-center">
+      <p>Berita</p>
+    </div>
+    <div class="row row-cols-md-1 row-cols-lg-2 row-cols-xl-2 row-cols-xxl-2">
+      <div class="col col-lg-6 col-xl-6 col-xxl-7">
+        <div class="list-group">
+          <li class="list-group-item bgAtasBerita text-white text-center" aria-current="true">
+            Index <b>Berita</b>
+          </li>
+          <a href="#" class="list-group-item list-group-item-action rounded-0 bgIndexBerita shadow p-3 mb-4">
+            <div class="row">
+              <div class="col-1 GambarPengumumanIndex">
+                <img src="/gambar/LogoLulus.jpg" alt="" class="mx-auto d-block" />
+              </div>
+              <div class="col-6">
+                <div class="d-flex justify-content-between PengumumanIndex">
+                  <p class="mb-1">
+                    Pengumuman Kelulusan Siswa Tahun Pelajaran 2023/2024
+                  </p>
+                </div>
+                <div>
+                  <small class="text-muted PenulisPengumumanIndex">SMK Negeri 4 Tasikmalaya</small>
+                </div>
+              </div>
+              <div class="col-5">
+                <small class="text-muted float-end PengumumanIndexDiterbitkan">06-05-2024</small>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div class="col col col-lg-6 col-xl-6 col-xxl-5">
+        <div class="list-group">
+          <li class="list-group-item bgAtasBerita text-white text-center" aria-current="true">
+            <b>Berita</b> Pilihan
+          </li>
+          <a href="#" class="list-group-item list-group-item-action rounded-0 bgBeritaPilihan shadow p-3 mb-4">
+            <div class="row">
+              <div class="col-1 GambarPengumumanPilihan">
+                <img src="/gambar/LogoLulus.jpg" alt="" class="mx-auto d-block" />
+              </div>
+              <div class="col-6">
+                <div class="d-flex justify-content-between PengumumanPilihan">
+                  <p class="mb-1">
+                    Pengumuman Kelulusan Siswa Tahun Pelajaran 2021/2022
+                  </p>
+                </div>
+                <div>
+                  <small class="text-muted PenulisPengumumanPilihan">SMK Negeri 4 Tasikmalaya</small>
+                </div>
+              </div>
+              <div class="col-5">
+                <small class="text-muted float-end PengumumanPilihanDiterbitkan">06-05-2024</small>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bgButton pt-3 pb-3">
+    <a href="https://smkn4tasikmalaya.vercel.app/Berita/" class="view-more btn">Lihat Berita Lainnya</a>
+  </div>
+</template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Beau+Rivage&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital@1&display=swap");
+
+.bgSambutan {
+  background-color: #eae7dd;
+}
+
+.bgSambutan .font p {
+  font-size: 4rem;
+  padding-top: 20px;
+  font-weight: bold;
+  font-family: "Beau Rivage", serif;
+}
+
+.bgSambutan .text-sambutan {
+  padding-top: 60px;
+  font-family: "IBM Plex Sans Condensed", serif;
+  font-weight: 400;
+  font-style: italic;
+  text-align: justify;
+}
+
+.bgInformasi {
+  background-color: #eae7dd;
+}
+
+.bgInformasi .judul-informasi p {
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "Beau Rivage", serif;
+}
+
+/* Gaya info box */
+.info-box {
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  margin: 10px;
+  padding: 20px;
+  text-align: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* Gaya judul info box */
+.info-box h2 {
+  font-size: 32px;
+  margin-bottom: 10px;
+  color: #007bff;
+  font-family: "Beau Rivage", serif;
+}
+
+/* Gaya jumlah */
+.info-box p {
+  font-size: 24px;
+  font-weight: bold;
+  margin: 0;
+  font-family: "Tinos", serif;
+}
+
+.bgGalery {
+  background-color: #eae7dd;
+}
+
+.bgGalery .judul-Galery p {
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "Beau Rivage", serif;
+}
+
+.bgButton {
+  background-color: #eae7dd;
+}
+
+.bgButton .view-more {
+  display: block;
+  margin: 0 auto;
+  padding: 12px 24px;
+  background: #c0b283;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  font-size: 14px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  width: 175px;
+}
+
+.bgButton .view-more:hover {
+  transform: scale(0.95);
+  background: #c0b283;
+  color: white;
+}
+
+.bgBerita {
+  background-color: #EAE7DD;
+}
+
+.bgBerita .judul-Berita p {
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "Beau Rivage", serif;
+}
+
+.bgIndexBerita {
+  background-color: #cdcdc0;
+}
+
+.bgIndexBerita .PengumumanIndex p {
+  color: #00008b;
+}
+
+.bgIndexBerita .PengumumanIndex p:hover {
+  color: #ff0000;
+}
+
+.bgBeritaPilihan {
+  background-color: #cdcdc0;
+}
+
+.bgBeritaPilihan .PengumumanPilihan p {
+  color: #00008b;
+}
+
+.bgBeritaPilihan .PengumumanPilihan p:hover {
+  color: #ff0000;
+}
+
+.bgAtasBerita {
+  background-color: #99775c;
+}
+
+@media (max-width: 575.98px) {
+  .info-box {
+    max-width: 100px;
+  }
+
+  .GambarPengumumanIndex img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+  .PengumumanIndex {
+    width: 400px;
+  }
+
+  .PengumumanIndex p {
+    font-size: 0.8rem;
+  }
+
+  .GambarPengumumanPilihan img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanPilihan {
+    width: 400px;
+  }
+
+  .PengumumanPilihan p {
+    font-size: 0.8rem;
+  }
+}
+
+@media (max-width: 767.98px) {
+  .info-box {
+    max-width: 500px;
+  }
+}
+
+@media (max-width: 991.98px) {
+  .info-box {
+    max-width: 200px;
+  }
+}
+
+@media (max-width: 1199.98px) {
+  .info-box {
+    max-width: 200px;
+  }
+}
+
+@media (max-width: 1399.98px) {
+  .info-box {
+    max-width: 500px;
+  }
+}
+
+@media (min-width: 576px) {
+  .GambarPengumumanIndex img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+  .PengumumanIndex {
+    width: 400px;
+  }
+
+  .PengumumanIndex p {
+    font-size: 0.8rem;
+  }
+
+  .GambarPengumumanPilihan img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanPilihan {
+    width: 400px;
+  }
+
+  .PengumumanPilihan p {
+    font-size: 0.8rem;
+  }
+}
+
+@media (min-width: 768px) {
+  .GambarPengumumanIndex img {
+    max-height: 7vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanIndex {
+    width: 400px;
+  }
+
+  .PengumumanIndex p {
+    font-size: 0.8rem;
+  }
+
+  .PenulisPengumumanIndex {
+    margin-left: 11px;
+    font-size: 0.68rem;
+  }
+
+  .PengumumanIndexDiterbitkan {
+    font-size: 0.75rem;
+  }
+
+  .GambarPengumumanPilihan img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanPilihan {
+    width: 400px;
+  }
+
+  .PengumumanPilihan p {
+    font-size: 0.8rem;
+  }
+}
+
+@media (min-width: 992px) {
+  .GambarPengumumanIndex img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanIndex {
+    width: 330px;
+  }
+
+  .PengumumanIndex p {
+    font-size: 0.77rem;
+    margin-left: 11px;
+  }
+
+  .PenulisPengumumanIndex {
+    margin-left: 11px;
+    font-size: 0.68rem;
+  }
+
+  .PengumumanIndexDiterbitkan {
+    font-size: 0.75rem;
+  }
+
+  .GambarPengumumanPilihan img {
+    max-height: 4.5vh;
+    min-height: 41px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanPilihan {
+    width: 330px;
+  }
+
+  .PengumumanPilihan p {
+    font-size: 0.77rem;
+    margin-left: 11px;
+  }
+
+  .PenulisPengumumanPilihan {
+    margin-left: 11px;
+    font-size: 0.68rem;
+  }
+
+  .PengumumanPilihanDiterbitkan {
+    font-size: 0.75rem;
+  }
+}
+
+@media (min-width: 1200px) {
+  .GambarPengumumanIndex img {
+    max-height: 5.5vh;
+    min-height: 50px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanIndex {
+    width: 510px;
+  }
+
+  .PengumumanIndex p {
+    font-size: 0.95rem;
+    margin-left: 20px;
+  }
+
+  .PenulisPengumumanIndex {
+    margin-left: 20px;
+    font-size: 0.80rem;
+  }
+
+  .PengumumanIndexDiterbitkan {
+    font-size: 0.75rem;
+    padding-top: 1px;
+  }
+
+  .GambarPengumumanPilihan img {
+    max-height: 5.5vh;
+    min-height: 50px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanPilihan {
+    width: 510px;
+  }
+
+  .PengumumanPilihan p {
+    font-size: 0.95rem;
+    margin-left: 20px;
+  }
+
+  .PenulisPengumumanPilihan {
+    margin-left: 20px;
+    font-size: 0.80rem;
+  }
+
+  .PengumumanPilihanDiterbitkan {
+    font-size: 0.75rem;
+    padding-top: 1px;
+  }
+}
+
+@media (min-width: 1400px) {
+  .GambarPengumumanIndex img {
+    max-height: 7vh;
+    min-height: 64px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanIndex {
+    width: 570px;
+  }
+
+  .PengumumanIndex p {
+    font-size: 1.2rem;
+    margin-left: 20px;
+  }
+
+  .PenulisPengumumanIndex {
+    margin-left: 20px;
+    font-size: 0.95rem;
+  }
+
+  .PengumumanIndexDiterbitkan {
+    font-size: 1.05rem;
+    padding-top: 1px;
+  }
+
+  .GambarPengumumanPilihan img {
+    max-height: 5.5vh;
+    min-height: 50px;
+    height: auto;
+    width: auto;
+    object-fit: contain;
+  }
+
+  .PengumumanPilihan {
+    width: 410px;
+  }
+
+  .PengumumanPilihan {
+    font-size: 0.8rem;
+  }
+
+  .PengumumanPilihan p {
+    font-size: 0.95rem;
+    margin-left: 15px;
+  }
+
+  .PenulisPengumumanPilihan {
+    margin-left: 15px;
+    font-size: 0.8rem;
+  }
+
+  .PengumumanPilihanDiterbitkan {
+    font-size: 0.75rem;
+    padding-top: 1px;
+  }
+}
+</style>
